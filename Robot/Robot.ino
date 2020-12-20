8c3e520b6f8604c124ebc92b853c6e9903c35394
whackamadoodle3000 <orca.pranav@gmail.com>
2020-12-19 22:09:35 -0800

    Update Robot.ino

--- Robot/Robot.ino
+++ Robot/Robot.ino
@@ -1,5 +1,6 @@
 #include <MPU6050.h>
 
+
 #include <VL53L0X.h>
 
 #include "CytronMotorDriver.h"
@@ -11,6 +12,12 @@ int motorRightA = 10;
 int motorRightB = 11;
 int mosfetPump = 5;
 int buzzer = 4;
+int IR_SENSOR = 0; // Sensor is connected to the analog A0
+
+MPU6050 mpu;
+
+int intSensorResult = 0; //Sensor result
+float fltSensorCalc = 0; //Calculated value
 
 bool sensed = false;
 bool handSensed = false;
@@ -20,14 +27,52 @@ CytronMD motorL(PWM_PWM, motorLeftA, motorLeftB);
 CytronMD motorR(PWM_PWM, motorRightA, motorRightB);
 VL53L0X sensor;
 
+void checkSettings()
+{
+  Serial.println();
+
+  Serial.print(" * Sleep Mode:            ");
+  Serial.println(mpu.getSleepEnabled() ? "Enabled" : "Disabled");
+
+  Serial.print(" * Clock Source:          ");
+  switch (mpu.getClockSource())
+  {
+    case MPU6050_CLOCK_KEEP_RESET:     Serial.println("Stops the clock and keeps the timing generator in reset"); break;
+    case MPU6050_CLOCK_EXTERNAL_19MHZ: Serial.println("PLL with external 19.2MHz reference"); break;
+    case MPU6050_CLOCK_EXTERNAL_32KHZ: Serial.println("PLL with external 32.768kHz reference"); break;
+    case MPU6050_CLOCK_PLL_ZGYRO:      Serial.println("PLL with Z axis gyroscope reference"); break;
+    case MPU6050_CLOCK_PLL_YGYRO:      Serial.println("PLL with Y axis gyroscope reference"); break;
+    case MPU6050_CLOCK_PLL_XGYRO:      Serial.println("PLL with X axis gyroscope reference"); break;
+    case MPU6050_CLOCK_INTERNAL_8MHZ:  Serial.println("Internal 8MHz oscillator"); break;
+  }
+
+  Serial.print(" * Accelerometer:         ");
+  switch (mpu.getRange())
+  {
+    case MPU6050_RANGE_16G:            Serial.println("+/- 16 g"); break;
+    case MPU6050_RANGE_8G:             Serial.println("+/- 8 g"); break;
+    case MPU6050_RANGE_4G:             Serial.println("+/- 4 g"); break;
+    case MPU6050_RANGE_2G:             Serial.println("+/- 2 g"); break;
+  }
+
+  Serial.print(" * Accelerometer offsets: ");
+  Serial.print(mpu.getAccelOffsetX());
+  Serial.print(" / ");
+  Serial.print(mpu.getAccelOffsetY());
+  Serial.print(" / ");
+  Serial.println(mpu.getAccelOffsetZ());
+
+  Serial.println();
+}
+
 
 void setup() {
   Serial.begin(9600);
   Serial.println("hi");
 
   pinMode(buzzer, OUTPUT);
-  pinMode(mosfetPump, OUTPUT); 
-  
+  pinMode(mosfetPump, OUTPUT);
+
   Wire.begin();
 
   sensor.setTimeout(500);
@@ -42,44 +87,78 @@ void setup() {
   // instead, provide a desired inter-measurement period in
   // ms (e.g. sensor.startContinuous(100)).
   sensor.startContinuous();
-  Serial.println("hi2");
+
+  while (!mpu.begin(MPU6050_SCALE_2000DPS, MPU6050_RANGE_2G))
+  {
+    Serial.println("yay");
+    delay(500);
+  }
+  mpu.calibrateGyro();
+  checkSettings();
 
 }
 
 void loop() {
+  // Handle the distance sensor
+  //Serial.print(sensor.readRangeContinuousMillimeters());
+  intSensorResult = analogRead(IR_SENSOR); //Get sensor value
+  fltSensorCalc = (6787.0 / (intSensorResult - 3.0)) - 4.0; //Calculate distance in cm
+
+  Serial.print(fltSensorCalc); //Send distance to computer
+  Serial.println(" cm"); //Add cm to result
+
+  //handle accelerometer
+  Vector normGyro = mpu.readNormalizeGyro();
+  Vector normAccel = mpu.readNormalizeAccel();
+
+  Serial.print(" Xnorm = ");
+  Serial.print(normGyro.XAxis);
+  Serial.print(" Ynorm = ");
+  Serial.print(normGyro.YAxis);
+  Serial.print(" Znorm = ");
+  Serial.println(normGyro.ZAxis);
+
+  //  Serial.print(" Xnorm = ");
+  //  Serial.print(normAccel.XAxis);
+  //  Serial.print(" Ynorm = ");
+  //  Serial.print(normAccel.YAxis);
+  //  Serial.print(" Znorm = ");
+  //  Serial.println(normAccel.ZAxis);
+  //
+  delay(10);
+
+
   // Handle the hand
-  Serial.print(sensor.readRangeContinuousMillimeters());
-  
-  handSensed = !sensor.timeoutOccurred() && sensor.readRangeContinuousMillimeters()<700 && sensed;
-  sensed = !sensor.timeoutOccurred() && sensor.readRangeContinuousMillimeters()<700;
-  if (handSensed){
-    motorL.setSpeed(0);   
-    motorR.setSpeed(0); 
+  handSensed = !sensor.timeoutOccurred() && sensor.readRangeContinuousMillimeters() < 700 && sensed;
+  sensed = !sensor.timeoutOccurred() && sensor.readRangeContinuousMillimeters() < 700;
+  if (handSensed) {
+    motorL.setSpeed(0);
+    motorR.setSpeed(0);
     beepCountdown++;
-    digitalWrite(buzzer,HIGH);
+    digitalWrite(buzzer, HIGH);
     //tone(buzzer, 1000); // Send 1KHz sound signal...
     delay(1000);
   }
-  else{
+  else {
     handSensed = false;
     beepCountdown = 0;
     //noTone(buzzer);
-    digitalWrite(buzzer,LOW);
+    digitalWrite(buzzer, LOW);
   }
 
 
   // Sanitize
-  if (beepCountdown>2){
+  if (beepCountdown > 2) {
     digitalWrite(mosfetPump, HIGH);
-    delay(2000);
+    delay(200);
     digitalWrite(mosfetPump, LOW);
   }
 
   //Drive
   Serial.println();
-  if (!handSensed && beepCountdown==0){
-      motorL.setSpeed(100);   
-      motorR.setSpeed(-100);  
+  if (!handSensed && beepCountdown == 0) {
+    motorL.setSpeed(105);
+    motorR.setSpeed(-100);
   }
 
 }
