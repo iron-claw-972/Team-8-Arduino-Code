3a0d5e4b875af0830ba520f45c5eb6fc3a9952ba
whackamadoodle3000 <orca.pranav@gmail.com>
2021-01-19 13:19:03 -0800

    rewritten

--- Robot/Robot.ino
+++ Robot/Robot.ino
@@ -1,3 +1,7 @@
+#include <SimpleTimer.h>
+
+#include <Encoder.h>
+
 #include <MPU6050.h>
 #include <math.h>
 
@@ -6,14 +10,14 @@
 #include "CytronMotorDriver.h"
 
 #include <Wire.h>
-int motorLeftA = 3;
-int motorLeftB = 9;
-int motorRightA = 10;
-int motorRightB = 11;
-int encoderLeftA = 8;
-int encoderLeftB = 12;
-int encoderRightA = 7;
-int encoderRightB = 6;
+const int motorLeftA = 3;
+const int motorLeftB = 9;
+const int motorRightA = 10;
+const int motorRightB = 11;
+const int encoderLeftA = 8;
+const int encoderLeftB = 12;
+const int encoderRightA = 7;
+const int encoderRightB = 6;
 
 
 int mosfetPump = 5;
@@ -23,19 +27,223 @@ int IR_SENSOR = 0; // Sensor is connected to the analog A0
 
 MPU6050 mpu;
 
-int intSensorResult = 0; //Sensor result
-float fltSensorCalc = 0; //Calculated value
-unsigned long myTime;
 
+float distanceUp = 0; //Calculated value
+unsigned long prevTime;
+int timeDiff = 1;
+double distanceForward;
+bool PIRup = false;
 
+//state variables
 bool sensed = false;
-bool handSensed = false;
-bool PIRSensed = false;
-int beepCountdown = 0;
 
+bool handSensed = false;
+bool underSomething = false;
+bool objectClose = false;
+bool objectTooClose = false;
+bool crashed = false;
+bool backingUp = false;
+bool turning = false;
+
+bool alarmActivated = true;
+bool alarmLoading = false;
+int buzzerCount = 0;
+
+int motorVelLeft = 100;
+int motorVelRight = 100;
+bool motorsStopped = false;
+
+double encoderLeft = 0;
+double encoderRight = 0;
+
+double encoderLeftStarting = 0;
+double encoderRightStarting = 0;
+
+
+static uint8_t prevNextCode = 0;
+static uint16_t store=0;
+
+static uint8_t prevNextCode1 = 0;
+static uint16_t store1=0;
+
+
+SimpleTimer timer;
 CytronMD motorL(PWM_PWM, motorLeftA, motorLeftB);
 CytronMD motorR(PWM_PWM, motorRightA, motorRightB);
 VL53L0X sensor;
+
+
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("hi");
+
+  pinMode(buzzer, OUTPUT);
+  pinMode(mosfetPump, OUTPUT);
+  pinMode(PIR, INPUT);
+
+  pinMode(encoderLeftA, INPUT_PULLUP);
+  pinMode(encoderLeftB, INPUT_PULLUP);
+  pinMode(encoderRightA, INPUT_PULLUP);
+  pinMode(encoderRightB, INPUT_PULLUP);
+
+  Wire.begin();
+
+  sensor.setTimeout(500);
+  if (!sensor.init())
+  {
+    Serial.println("Failed to detect and initialize sensor!");
+
+  }
+
+  // Start continuous back-to-back mode (take readings as
+  // fast as possible).  To use continuous timed mode
+  // instead, provide a desired inter-measurement period in
+  // ms (e.g. sensor.startContinuous(100)).
+  sensor.startContinuous(4);
+
+  while (!mpu.begin(MPU6050_SCALE_2000DPS, MPU6050_RANGE_2G))
+  {
+    Serial.println("yay");
+    delay(500);
+  }
+  mpu.calibrateGyro();
+  checkSettings();
+  delay(500);
+
+  timer.setInterval(100, updateState);
+
+}
+
+void updateState(){
+  // Handle the distance sensor  
+  distanceUp = (6787.0 / (analogRead(IR_SENSOR) - 3.0)) - 4.0; //Calculate distance in cm
+  objectClose = distanceUp<26 and distanceUp>10;
+  objectTooClose = distanceUp<10;
+
+
+  // Handle the up sensors
+  distanceUp=sensor.readRangeContinuousMillimeters();
+  PIRup=digitalRead(PIR);
+  handSensed = !sensor.timeoutOccurred() && distanceUp < 700 && sensed && PIRup;
+  underSomething = !sensor.timeoutOccurred() && distanceUp < 700 && sensed && !PIRup;
+  sensed = !sensor.timeoutOccurred() && distanceUp < 700;
+
+  // Handle the spray / buzz
+  if (handSensed){
+    alarm();
+    motorsStopped = true;
+    if (buzzerCount>3){
+      pump();
+      buzzerCount=0;
+      motorsStopped = false;
+    }
+  }
+  else{
+    buzzerCount = 0;
+    motorsStopped = false;
+  }
+  
+ //Drive
+ if (objectTooClose or crashed or underSomething){
+  if (not backingUp){
+    encoderLeftStarting = encoderLeft;
+    encoderRightStarting = encoderRight;
+  }
+  backingUp = true;
+  turning = false;
+ }
+ else if (objectClose){
+  if (not turning and not backingUp){
+    encoderLeftStarting = encoderLeft;
+    encoderRightStarting = encoderRight;
+  }
+  turning = true;
+ }
+ 
+ if (not motorsStopped and not turning and not backingUp){
+   forward(sqrt(distanceUp)*20+10);
+ }
+ else if (not motorsStopped and turning){
+   turning = turn(0.5, encoderLeftStarting, encoderLeft);
+ }
+ else if (not motorsStopped and objectTooClose){
+   backingUp = backUp(-0.3, encoderLeftStarting, encoderLeft);
+   turning=false;
+ }
+ if (motorsStopped){
+  stopMotors();
+ }
+}
+
+void loop() {
+  
+  
+  // Handle encoder
+  encoderLeft += -read_rotary()/63.0;
+  encoderRight += -read_rotary1()/78.0;
+  //Serial.println(encoderRight);
+  timer.run();
+
+}
+
+void stopMotors(){
+  motorL.setSpeed(0);
+  motorR.setSpeed(0);
+}
+
+void forward(double vel){
+  motorL.setSpeed(vel);
+  motorR.setSpeed(-vel);
+}
+bool turn(double rotations, double start, double current){
+  if ((start+rotations)>current){
+    motorL.setSpeed(150);
+    motorR.setSpeed(0);
+    return true;
+  }
+  else{
+    return false;
+  }
+}
+bool backUp(double rotations, double start, double current){
+  if ((start+rotations)<current){
+    motorL.setSpeed(-150);
+    motorR.setSpeed(150);
+    return true;
+  }
+  else{
+    return false;
+  }
+  
+}
+void pump(){
+  digitalWrite(mosfetPump, HIGH);
+  timer.setTimeout(150,[](){digitalWrite(mosfetPump, LOW);});
+}
+void alarm(){
+  if (!alarmLoading){
+    if (alarmActivated){
+      digitalWrite(buzzer, HIGH);
+      buzzerCount++;
+      timer.setTimeout(1000,[](){digitalWrite(buzzer, LOW);alarmActivated=false;alarmLoading=false;});
+      alarmLoading=true;
+    }
+    else{
+      timer.setTimeout(200,[](){alarmActivated=true;alarmLoading=false;});
+      alarmLoading=true;
+    }
+  }
+}
+
+double getRotation(double currentAngle, int timeDiff){
+    Vector normGyro = mpu.readNormalizeGyro();
+    currentAngle+=normGyro.ZAxis * timeDiff/1000.0;
+    currentAngle=fmod(currentAngle,360.0);
+    return currentAngle;
+}
+
+
 
 void checkSettings()
 {
@@ -75,132 +283,43 @@ void checkSettings()
   Serial.println();
 }
 
+// A vald CW or  CCW move returns 1, invalid returns 0.
+int8_t read_rotary() {
+  static int8_t rot_enc_table[] = {0,1,1,0,1,0,0,1,1,0,0,1,0,1,1,0};
 
-void setup() {
-  Serial.begin(9600);
-  Serial.println("hi");
+  prevNextCode <<= 2;
+  if (digitalRead(encoderLeftA)) prevNextCode |= 0x02;
+  if (digitalRead(encoderLeftB)) prevNextCode |= 0x01;
+  prevNextCode &= 0x0f;
 
-  pinMode(buzzer, OUTPUT);
-  pinMode(mosfetPump, OUTPUT);
-  pinMode(PIR, INPUT);
-
-  pinMode(encoderLeftA, INPUT);
-  pinMode(encoderLeftB, INPUT);
-  pinMode(encoderRightA, INPUT);
-  pinMode(encoderRightB, INPUT);
-
-  Wire.begin();
-
-  sensor.setTimeout(500);
-  if (!sensor.init())
-  {
-    Serial.println("Failed to detect and initialize sensor!");
-
-  }
-
-  // Start continuous back-to-back mode (take readings as
-  // fast as possible).  To use continuous timed mode
-  // instead, provide a desired inter-measurement period in
-  // ms (e.g. sensor.startContinuous(100)).
-  sensor.startContinuous(4);
-
-  while (!mpu.begin(MPU6050_SCALE_2000DPS, MPU6050_RANGE_2G))
-  {
-    Serial.println("yay");
-    delay(500);
-  }
-  mpu.calibrateGyro();
-  checkSettings();
-  delay(500);
-
+   // If valid then store as 16 bit data.
+   if  (rot_enc_table[prevNextCode] ) {
+      store <<= 4;
+      store |= prevNextCode;
+      //if (store==0xd42b) return 1;
+      //if (store==0xe817) return -1;
+      if ((store&0xff)==0x2b) return -1;
+      if ((store&0xff)==0x17) return 1;
+   }
+   return 0;
 }
 
+int8_t read_rotary1() {
+  static int8_t rot_enc_table[] = {0,1,1,0,1,0,0,1,1,0,0,1,0,1,1,0};
 
-double getRotation(double currentAngle, int timeDiff){
-    Vector normGyro = mpu.readNormalizeGyro();
-    currentAngle+=normGyro.ZAxis * timeDiff/1000.0;
-    currentAngle=fmod(currentAngle,360.0);
-    return currentAngle;
-}
+  prevNextCode1 <<= 2;
+  if (digitalRead(encoderRightA)) prevNextCode1 |= 0x02;
+  if (digitalRead(encoderRightB)) prevNextCode1 |= 0x01;
+  prevNextCode1 &= 0x0f;
 
-
-void turn(double targetAngle){
-  int timeDiff = 0;
-  double currentAngle = 0;
-  double angleDiff;
-  double rotationSpeed;
-  unsigned long myTime = millis();
-  while (((currentAngle-targetAngle)>4) or ((currentAngle-targetAngle)<-4)){
-    angleDiff=targetAngle-currentAngle;
-    rotationSpeed = sqrt(sqrt(abs(angleDiff/180))) * 200 * angleDiff/abs(angleDiff);
-    
-    motorL.setSpeed(-rotationSpeed);
-    motorR.setSpeed(-(rotationSpeed));
-    delay(30);
-    
-    timeDiff = millis() - myTime;
-    myTime = millis();
-    currentAngle = getRotation(currentAngle,timeDiff);
-    Serial.println(angleDiff);
-
-  }
-  motorL.setSpeed(0);
-  motorR.setSpeed(0);
-  
-}
-
-void loop() {
-  
-  
-  // Handle the distance sensor
-  //Serial.print(sensor.readRangeContinuousMillimeters());
-  
-  intSensorResult = analogRead(IR_SENSOR); //Get sensor value
-  fltSensorCalc = (6787.0 / (intSensorResult - 3.0)) - 4.0; //Calculate distance in cm
-  
-//  Serial.print(fltSensorCalc); //Send distance to computer
-//  Serial.println(" cm"); //Add cm to result
-
-  
-  if (fltSensorCalc<20) {
-    turn(90);
-  }
-
-  // Handle the hand
-  PIRSensed = digitalRead(PIR);
-  handSensed = !sensor.timeoutOccurred() && sensor.readRangeContinuousMillimeters() < 700 && sensed && PIRSensed;
-  sensed = !sensor.timeoutOccurred() && sensor.readRangeContinuousMillimeters() < 700;
-  if (handSensed) {
-    motorL.setSpeed(0);
-    motorR.setSpeed(0);
-    beepCountdown++;
-    //digitalWrite(buzzer, HIGH);
-    //tone(buzzer, 1000); // Send 1KHz sound signal...
-    delay(1000);
-    digitalWrite(buzzer, LOW);
-  }
-  else {
-    handSensed = false;
-    beepCountdown = 0;
-    //noTone(buzzer);
-  }
-
-
-  // Sanitize
-  if (beepCountdown > 2) {
-    //digitalWrite(mosfetPump, HIGH);
-    delay(150);
-    digitalWrite(mosfetPump, LOW);
-    delay(1000);
-  }
-
-
-  //Drive
-  
-  if (!handSensed && beepCountdown == 0) {
-    motorL.setSpeed(sqrt(fltSensorCalc)*20+10);
-    motorR.setSpeed(-sqrt(fltSensorCalc)*20+10);
-  }
-  delay(30);
-
+   // If valid then store as 16 bit data.
+   if  (rot_enc_table[prevNextCode1] ) {
+      store1 <<= 4;
+      store1 |= prevNextCode1;
+      //if (store1==0xd42b) return 1;
+      //if (store1==0xe817) return -1;
+      if ((store1&0xff)==0x2b) return -1;
+      if ((store1&0xff)==0x17) return 1;
+   }
+   return 0;
 }
